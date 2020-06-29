82ade8dd3461934d9d7cf9d58065a94a5d51cdce
Sasha Firsov <suns@firsov.net>
2020-06-28 20:16:53 -0700

    embedding index.html into AEM component

--- ui.frontend/post-js-build.js
+++ ui.frontend/post-js-build.js
@@ -1,5 +1,7 @@
+const CDN_ROOT = "/etc.clientlibs/wc-in-aem/clientlibs/clientlib-site/resources/build";
 const path = require('path');
 const fs = require('fs-extra');
+const isDir = path => { try{ return fs.lstatSync(path).isDirectory(); }catch(e){ return false; } }
 const BUILD_DIR = path.join(__dirname, 'build');
 
 const RES_DIR = path.join(__dirname, 'dist','clientlib-site');
@@ -7,4 +9,28 @@ const RES_BUILD = path.join(RES_DIR,'build');
 fs.mkdirsSync( RES_DIR );
 
 fs.removeSync( RES_BUILD );
+
+
+fs.readdirSync( BUILD_DIR )
+.filter( d => isDir( path.join( BUILD_DIR, d ) ) )
+.forEach( d =>
+{
+    const indexFilePath = path.join(BUILD_DIR,d, 'index.html');
+    const txt = fs.readFileSync(indexFilePath, 'utf8')
+                    .replace( /src=\"/g , `src="${CDN_ROOT}/${d}/` )
+                    .replace( /<html>/   , `` )
+                    .replace( /<\/html>/ , `` )
+                    .replace( /<head>/   , `` )
+                    .replace( /<\/head>/ , `` )
+                    .replace( /<body>/   , `` )
+                    .replace( /<\/body>/ , `` );
+    fs.moveSync( indexFilePath, indexFilePath+'0' );
+    fs.writeFileSync( indexFilePath, txt );
+});
+
 fs.moveSync( BUILD_DIR, RES_BUILD , true );
+
+const COMP_DIR = path.join(__dirname, "components");
+fs.mkdirsSync( RES_DIR );
+
+fs.copySync( COMP_DIR , path.join(RES_DIR,'components') );
